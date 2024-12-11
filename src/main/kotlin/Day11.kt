acb80c2d84cd655542ebbe66aba63d0900d19eab
MVaik <105720462+MVaik@users.noreply.github.com>
2024-12-11 23:04:42 +0200

    chore: cleaned up day11 solution

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -10,23 +10,23 @@ class Day11 {
       }
       for (i in 0..<blinkCount) {
         val newNums = HashMap<String, Long>()
-        for ((value) in nums) {
+        for ((value, count) in nums) {
           when {
             value.toLong() == 0L -> {
-              newNums["1"] = newNums.getOrDefault("1", 0L) + nums[value]!!
+              newNums["1"] = newNums.getOrDefault("1", 0L) + count
             }
 
             value.length % 2 == 0 -> {
               val halfwayPoint = value.length / 2
               val firstHalf = value.slice(0..<halfwayPoint)
-              val secondHalf = value.slice(halfwayPoint..<value.length).toLong().toString()
-              newNums[firstHalf] = newNums.getOrDefault(firstHalf, 0L) + nums[value]!!
-              newNums[secondHalf] = newNums.getOrDefault(secondHalf, 0L) + nums[value]!!
+              val secondHalf = value.slice(halfwayPoint..<value.length)
+              newNums[firstHalf] = newNums.getOrDefault(firstHalf, 0L) + count
+              newNums[secondHalf] = newNums.getOrDefault(secondHalf, 0L) + count
             }
 
             else -> {
               val newVal = (value.toLong() * 2024L).toString()
-              newNums[newVal] = newNums.getOrDefault(newVal, 0L) + nums[value]!!
+              newNums[newVal] = newNums.getOrDefault(newVal, 0L) + count
             }
           }
         }
