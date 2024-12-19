b9d90044899922d0e85f33beea64548df48414a0
MVaik <105720462+MVaik@users.noreply.github.com>
2024-12-20 00:08:10 +0200

    feat: added day 19 part 2

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -2,10 +2,10 @@ import shared.Utils
 
 class Day19 {
   companion object {
-    private fun getInput(): Pair<List<String>, Set<String>>? {
+    private fun getInput(): Pair<Set<String>, Set<String>>? {
       val input = Utils.readText("/inputs/day19.txt") ?: return null
       val (towelsRow, designs) = input.split("\r\n\r\n")
-      val towels = towelsRow.split(", ")
+      val towels = towelsRow.split(", ").toSet()
       val designsArr = designs.split("\r\n").toSet()
       return Pair(towels, designsArr)
     }
@@ -21,8 +21,41 @@ class Day19 {
       }
       println(possibleDesigns)
     }
+
+    private fun countTowelPossibilities(
+      input: String,
+      towels: Set<String>,
+      knownPossibilities: HashMap<String, Long>
+    ): Long {
+      if (knownPossibilities.contains(input)) {
+        return knownPossibilities[input]!!
+      }
+      var possibilities = 0L
+      if (towels.contains(input)) {
+        possibilities++
+      }
+      for (i in 1..<input.length) {
+        if (towels.contains(input.slice(0..<i))) {
+          val count = countTowelPossibilities(input.slice(i..<input.length), towels, knownPossibilities)
+          possibilities += count
+        }
+      }
+      knownPossibilities[input] = possibilities
+      return possibilities
+    }
+
+    fun solvePartTwo() {
+      val (towels, designs) = getInput() ?: return
+      var possibleDesigns = 0L
+      val knownPossibilities = HashMap<String, Long>()
+      for (design in designs) {
+        possibleDesigns += countTowelPossibilities(design, towels, knownPossibilities)
+      }
+      println(possibleDesigns)
+    }
   }
 }
 
 fun main() {
+  Day19.solvePartTwo()
 }
