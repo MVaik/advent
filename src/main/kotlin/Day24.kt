ee518a7856d125a46eb640e9916f284be4420b93
MVaik <105720462+MVaik@users.noreply.github.com>
2024-12-25 13:35:32 +0200

    feat: added day 24 part 2 solution

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -18,13 +18,18 @@ class Day24 {
       return Pair(values, parsedTransformations)
     }
 
-    fun solvePartOne() {
+    fun solvePartOneAndTwo() {
       val (values, transformations) = getInput() ?: return
       val queue = ArrayDeque<List<String>>()
       for (transformation in transformations) {
         queue.add(transformation)
       }
-
+      val orInputs = mutableSetOf<String>()
+      val faultyXorOperations = mutableSetOf<List<String>>()
+      val faultyZOutputs = mutableSetOf<List<String>>()
+      val andOutputs = mutableSetOf<List<String>>()
+      val xorInputs = mutableSetOf<String>()
+      val xorOutputs = mutableSetOf<String>()
       while (queue.size > 0) {
         val curr = queue.removeFirst()
         if (!values.contains(curr[0]) || !values.contains(curr[2])) {
@@ -36,14 +41,30 @@ class Day24 {
         when (curr[1]) {
           "AND" -> {
             values[curr[3]] = if (firstValue == 1 && firstValue == secondValue) 1 else 0
+            andOutputs.add(curr)
+            if (curr[3].startsWith("z") && curr[3] != "z45") {
+              faultyZOutputs.add(curr)
+            }
           }
 
           "OR" -> {
             values[curr[3]] = if (firstValue == 1 || secondValue == 1) 1 else 0
+            orInputs.addAll(listOf(curr[0], curr[2]))
+            if (curr[3].startsWith("z") && curr[3] != "z45") {
+              faultyZOutputs.add(curr)
+            }
           }
 
           "XOR" -> {
             values[curr[3]] = if (firstValue != secondValue) 1 else 0
+            val isValidInput = listOf(curr[0], curr[2]).all { it.startsWith("x") || it.startsWith("y") }
+            val isValidOutput = curr[3].startsWith("z")
+            xorInputs.addAll(listOf(curr[0], curr[2]))
+            if (!isValidInput && !isValidOutput) {
+              faultyXorOperations.add(curr)
+            } else if (isValidInput && listOf(curr[0], curr[2], curr[3]).none { it.endsWith("00") }) {
+              xorOutputs.add(curr[3])
+            }
           }
         }
       }
@@ -51,10 +72,22 @@ class Day24 {
         values.filterKeys { key -> key.startsWith("z") }.toList().sortedByDescending { it.first }
           .joinToString("") { it.second.toString() }.toLong(2)
       println("Result: $valueFromBits")
+
+      val faultyAndOutputs = andOutputs.filter { outputRow ->
+        listOf(outputRow[0], outputRow[2]).none { it.startsWith("x00") || it.startsWith("y00") } && !orInputs.contains(
+          outputRow[3]
+        )
+      }
+      val faultyXorOutputs = xorOutputs.filter { output -> !xorInputs.contains(output) }
+      val faultyWires = listOf(faultyAndOutputs.map { it[3] },
+        faultyXorOperations.map { it[3] },
+        faultyXorOutputs,
+        faultyZOutputs.map { it[3] }).flatten().toSortedSet().joinToString(",")
+      println("Faulty wires: $faultyWires")
     }
   }
 }
 
 fun main() {
-  Day24.solvePartOne()
+  Day24.solvePartOneAndTwo()
 }
