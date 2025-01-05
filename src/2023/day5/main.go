508a62d428034919b82abd0cf97b241d29b076f7
MVaik <105720462+MVaik@users.noreply.github.com>
2025-01-05 14:55:29 +0200

    feat: added 2023 day 5 full solution

--- src/2023/day5/main.go
+++ src/2023/day5/main.go
@@ -15,7 +15,11 @@ func main() {
 		parsedInts := utils.MatchIntsAndParse(sections[i])
 		parsedRange := make([][]int, 0, len(parsedInts)/3)
 		for j := 0; j < len(parsedInts)-2; j += 3 {
-			parsedRange = append(parsedRange, parsedInts[j:j+3])
+			destinationStart := parsedInts[j]
+			sourceStart := parsedInts[j+1]
+			rangeLength := parsedInts[j+2]
+			// Add ranges as start, end instead of start, length
+			parsedRange = append(parsedRange, []int{sourceStart, sourceStart + rangeLength - 1, destinationStart, destinationStart + rangeLength - 1})
 		}
 		parsedSections = append(parsedSections, parsedRange)
 	}
@@ -23,34 +27,60 @@ func main() {
 	queue := make([][]int, 0, len(seeds))
 
 	for _, seed := range seeds {
-		queue = append(queue, []int{0, seed, 1})
+		queue = append(queue, []int{1, 0, seed, seed})
+	}
+
+	for i := 0; i < len(seeds); i += 2 {
+		// Add seeds as start, end instead of start, length
+		queue = append(queue, []int{2, 0, seeds[i], seeds[i] + seeds[i+1] - 1})
 	}
 
 	partOneLowest := -1
+	partTwoLowest := -1
 
 	for len(queue) > 0 {
 		curr := queue[0]
 		queue = queue[1:]
+		part := curr[0]
+		level := curr[1]
+		currStart := curr[2]
+		currEnd := curr[3]
 
-		if curr[0] == 7 {
-			if partOneLowest == -1 || partOneLowest > curr[1] {
-				partOneLowest = curr[1]
+		if level == 7 {
+			if part == 1 && (partOneLowest == -1 || partOneLowest > currStart) {
+				partOneLowest = currStart
+			}
+			if part == 2 && (partTwoLowest == -1 || partTwoLowest > currStart) {
+				partTwoLowest = currStart
 			}
 			continue
 		}
 		rangeFound := false
-		for _, potentialRange := range parsedSections[curr[0]] {
-			// If the current number fits between our range, add the transformation to the queue
-			if curr[1] >= potentialRange[1] && curr[1] <= potentialRange[1]+potentialRange[2] {
-				newVal := potentialRange[0] + curr[1] - potentialRange[1]
-				queue = append(queue, []int{curr[0] + 1, newVal, curr[2]})
+		for _, potentialRange := range parsedSections[level] {
+			sourceStart := potentialRange[0]
+			sourceEnd := potentialRange[1]
+			destinationStart := potentialRange[2]
+			destinationEnd := potentialRange[3]
+
+			// Do the ranges overlap?
+			if currEnd >= sourceStart && sourceEnd >= currStart {
+				// Add the regular piece that is contained in source range, accounting for any diffs in start or end
+				queue = append(queue, []int{part, level + 1, destinationStart + max(currStart-sourceStart, 0), destinationEnd - max(sourceEnd-currEnd, 0)})
+
+				if currStart < sourceStart {
+					// Send unmatched piece back to checking the same level
+					queue = append(queue, []int{part, level, currStart, sourceStart - 1})
+				} else if currEnd > sourceEnd {
+					queue = append(queue, []int{part, level, sourceEnd + 1, currEnd})
+				}
 				rangeFound = true
 			}
 		}
-		// Add the number itself back into the queue for next transformation if it didn't have any matching ranges
+		// Keep the unchanged range if nothing matched
 		if !rangeFound {
-			queue = append(queue, []int{curr[0] + 1, curr[1], curr[2]})
+			queue = append(queue, []int{part, level + 1, currStart, currEnd})
 		}
 	}
 	println("Part one result: ", partOneLowest)
+	println("Part two result: ", partTwoLowest)
 }
--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module github.com/MVaik/advent/src
 
 go 1.23.4
+
+require github.com/go-delve/delve v1.24.0 // indirect
